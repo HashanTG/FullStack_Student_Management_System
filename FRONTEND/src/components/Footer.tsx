5447e7c024bbe500db6cb64161f823eb598dca9d
Hashan Tharanga Gamage <109969514+HashanTG@users.noreply.github.com>
2025-01-19 16:46:14 +0530

    i change the footer of the code

--- FRONTEND/src/components/Footer.tsx
+++ FRONTEND/src/components/Footer.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
+
 const Footer: React.FC = () => {
- return (
-    <footer className="bg-dark text-center text-white mt-auto py3">
-        <div>© 2024 Employee Management System</div>
+  return (
+    <footer className="bg-dark text-center text-white mt-auto py-4">
+      <div className="container">
+        <p className="mb-1">&copy; 2024 Employee Management System. All Rights Reserved.</p>
+        <ul className="list-inline">
+          <li className="list-inline-item">
+            <a href="#" className="text-white">Privacy Policy</a>
+          </li>
+          <li className="list-inline-item">
+            <a href="#" className="text-white">Terms of Service</a>
+          </li>
+          <li className="list-inline-item">
+            <a href="#" className="text-white">Contact Us</a>
+          </li>
+        </ul>
+        <div className="mt-3">
+          <a href="#" className="text-white me-3">
+            <i className="fab fa-facebook-f"></i>
+          </a>
+          <a href="#" className="text-white me-3">
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a href="#" className="text-white">
+            <i className="fab fa-linkedin-in"></i>
+          </a>
+        </div>
+      </div>
     </footer>
- );
+  );
 };
+
 export default Footer;
