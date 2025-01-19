b6da42e3fb734cf88f2a44cdc764df3922881cff
Hashan Tharanga Gamage <109969514+HashanTG@users.noreply.github.com>
2025-01-19 16:37:17 +0530

    this is i change the header component

--- FRONTEND/src/components/Header.tsx
+++ FRONTEND/src/components/Header.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
+
 const Header: React.FC = () => {
- return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-black">
-        <div className="container">
-            <a className="navbar-brand" href="#">
-            Employee Management System
-            </a>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-3">
+      <div className="container d-flex justify-content-between">
+        <a className="navbar-brand text-white fw-bold" href="#">
+          <i className="fas fa-users me-2"></i>
+          Employee Management System
+        </a>
+        <div>
+          <button className="btn btn-outline-light me-2" type="button">Login</button>
+          <button className="btn btn-light" type="button">Sign Up</button>
         </div>
+      </div>
     </nav>
- );
+  );
 };
+
 export default Header;
